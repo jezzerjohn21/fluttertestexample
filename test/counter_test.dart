8d509d46234e1b4dc9d663ccb75992d114d03203
Paul John toral <pauljohnramireztoral@gmail.com>
2024-02-26 14:38:33 +0800

    added incremental resource

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,40 +2,65 @@ import "package:flutter_test/flutter_test.dart";
 import "package:fluttertestexample/counter.dart";
 
 main() {
-  late Counter counter = Counter();
+  late Counter counter;
+
+  setUp(() {
+    counter = Counter();
+  });
 
   group('Counter class', () {
-    setUp(() => counter = Counter());
-
     test('Given Counter class, when instantiated, them count = 0', () {
-//arrange
-
-//act
+      counter = Counter();
       int count = counter.count;
-//assert
+
       expect(count, 0);
     });
 
-    test(
-        'Given Counter class is instantiated, when increamentCount is called, then count = 1',
-        () {
-//arrange
+    group('Increment ', () {
+      late Counter counter;
 
-//act
-      counter.incrementCount();
-//assert
-      expect(counter.count, 1);
+      setUpAll(() {
+        //using setUpAll to implement shared resource counter
+        counter = Counter();
+      });
+
+      test(
+        'Given Counter class is instantiated, when incrementCount is called once, then count++',
+        () {
+          counter.incrementCount();
+          expect(counter.count, 1);
+        },
+      );
+
+      test(
+        'Given Counter class is instantiated, when incrementCount is called twice, then count++',
+        () {
+          counter.incrementCount();
+          expect(counter.count, 2);
+        },
+      );
+
+      test(
+        'Given Counter class is instantiated, when incrementCount is called twice, then count++',
+        () {
+          counter.incrementCount();
+          expect(counter.count, 3);
+        },
+      );
+
+      tearDownAll(() {
+        counter = Counter();
+      });
     });
 
-    test(
-        'given Counter class is instantiated, when icreamentCount is called, then count = -1',
-        () {
-//arrange
+    group('Decrement', () {
+      test(
+          'given Counter class is instantiated, when decrementCount is called, then count = -1',
+          () {
+        counter.decrementCount();
 
-//act
-      counter.decrementCount();
-//assert
-      expect(counter.count, -1);
+        expect(counter.count, -1);
+      });
     });
   });
 
