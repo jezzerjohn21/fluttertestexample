950b2026906dda671e7e0df219371250e0adfd2e
Paul John toral <pauljohnramireztoral@gmail.com>
2024-02-26 14:14:34 +0800

    implemented reset

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -1,61 +1,50 @@
 import "package:flutter_test/flutter_test.dart";
 import "package:fluttertestexample/counter.dart";
 
-
 main() {
- late Counter counter = Counter(); 
+  late Counter counter = Counter();
 
+  group('Counter class', () {
+    setUp(() => counter = Counter());
 
- group('Counter class', () { 
-
- setUp(()=>
- counter = Counter());
-  
-  test('Given Counter class, when instantiated, them count = 0', () {
+    test('Given Counter class, when instantiated, them count = 0', () {
 //arrange
 
 //act
-    int count = counter.count;
+      int count = counter.count;
 //assert
-    expect(count, 0);
+      expect(count, 0);
+    });
+
+    test(
+        'Given Counter class is instantiated, when increamentCount is called, then count = 1',
+        () {
+//arrange
+
+//act
+      counter.incrementCount();
+//assert
+      expect(counter.count, 1);
+    });
+
+    test(
+        'given Counter class is instantiated, when icreamentCount is called, then count = -1',
+        () {
+//arrange
+
+//act
+      counter.decrementCount();
+//assert
+      expect(counter.count, -1);
+    });
   });
 
+  test(
+      'given Counter class is instantiated,and  when increament is called, and reset is called,  then count = 0',
+      () {
+    counter.incrementCount();
+    counter.reset();
 
-  test('Given Counter class is instantiated, when increamentCount is called, then count = 1', (){
-//arrange
-
-//act
-counter.incrementCount();
-//assert
-expect(counter.count, 1);
-
+    expect(counter.count, 0);
   });
-
-
-test('given Counter class is instantiated, when icreamentCount is called, then count = -1', (){
-
-//arrange
-
-//act
-counter.decrementCount();
-//assert
-expect(counter.count, -1);
-});
-
-});
-
-
-test('given Counter class is instantiated,and  when increament is called, and reset is called,  then count = 0',(){
-
-counter.incrementCount();
-counter.reset();
-
-
-expect(counter.count, throw UnimplementedError);
-
-
-});
-
-
 }
-
